0a08327c09fb6035025094b3ccb3e3a5f9a41072
Gaspard <gaspard@courchinoux.org>
2023-02-17 20:03:06 +0100

    ajout du code , faire dwg

---
+++ Main.cpp
@@ -0,0 +1,72 @@
+// COPYRIGHT 2023 Gaspard COURCHINOUX all rights reserved
+
+#include <iostream>
+
+#define PI  3.14159265
+
+void printf_main_info() {
+	
+	std::cout << "Copyright 2023 , Gaspard COURCHINOUX Tous droits réservés " << std::endl;
+	
+	std::cout << "Toutes les dimensions sont exprimées en millimètres" << std::endl;
+	
+	
+	}
+unsigned long d  = 0; 
+unsigned long da = 0; 
+double df = 0; 
+unsigned long ha = 0 ; 
+double hf = 0 ; 
+double p = 0; 
+double s_e = 0;
+void calculer(char**argv){
+	
+	unsigned long module = atol(argv[1]);
+	unsigned long dents = atol(argv[2]);
+	
+	
+	printf("juste pour confirmation , il y a  %ld  dents et module %ld \n",dents,module);
+	
+	
+	printf("D (diametre) %lu en mm\n",module*dents);
+	d = module*dents;
+	printf("Da (diametre de tete) %lu en mm\n",module*dents+2);
+	da = module*dents+2;
+	printf("Df (diametre de pied) %f en mm\n",module*dents-2.5);
+	df = module*dents-2.5;
+	
+	printf("Ha (hauteur de tete) %lu en mm\n",module);
+	ha = module; 
+	
+		printf("Hf (hauteur de pied) %f en mm\n",1.25*module);
+		hf = 1.2*module; 
+		
+		printf("P (pas linéaire) %f en mm\n",module*PI);
+		p = module*PI;
+		printf("S=E (épaisseur de dent = largeur du vide) %f en mm\n",module*PI/2);
+		s_e = module*PI/2;
+	}
+
+int main(int argc,char**argv) {
+	
+	printf_main_info();
+	
+	if(argc < 2) {
+		
+		std::cout << "USAGE : " << "./dents  [MODULE] [NOMBRE_DE_DENTS]" << std::endl;
+		exit(1);
+		
+		}
+	
+	calculer(argv);
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,80 @@
+# objet du présent programme 
+
+
+Ce programme permet de calculer les dents à developpante du cercle d'un engrenage. 
+
+Il se destine à une application pour l'horlogerie en priorité. 
+
+il permet de calculer un diamétre avec seulement un module et un nombre de dents. 
+
+Plus tard, j'aimerai générer des fichiers dwg pour directement importer dans solidworks et catia. 
+
+
+# Compilation
+
+
+
+ce programme utilise le générateur CMAKE permettant de générer des solutions visual studio ou Makefile pour compiler le programme. 
+
+
+
+## pour windows 
+
+
+Pour windows , il faut visual studio 2022 avec visual c++. 
+
+
+
+Ensuite aller à la racine des dossiers : 
+
+```
+
+mkdir build 
+cd build
+cmake ..
+
+```
+
+Ensuite , double cliquer sur la nom de la solution et générer le code objet. 
+
+Un fichier executable sera disponible. 
+
+
+
+## pour unix
+
+
+Pour les systèmes unix (linux,macos, *bsd) ,le procédé est moins couteux en terme d'espace disque. 
+
+
+Il faut un compilateur (gcc et llvm font l'affaire). 
+
+
+Consulter le site de la distribution linux ou bsd pour installer un compilateur. 
+
+
+Généralement gcc/g++ sera installé sous linux tandis que sur *bsd/macos cela sera llvm. 
+
+
+Je préfére llvm. Mais la compilation devrait fonctionner sous g++. 
+
+
+```
+
+cd [SOURCE] 
+mkdir build 
+cd build 
+cmake .. 
+make
+
+```
+
+
+Un executable sera produit. 
+
+# usage : 
+
+```
+./dents [MODULE] [NOMBRE_DE_DENTS]
+```
+
